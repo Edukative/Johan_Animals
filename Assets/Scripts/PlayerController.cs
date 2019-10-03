ba28810419472137c97370ccee4105c5d8ce6020
Edukative <56083723+Edukative@users.noreply.github.com>
2019-10-03 16:03:26 +0200

    Added Johan's 2nd project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private float horizontalInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move left/right
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+        if (transform.position.x < -15)
+        {
+            transform.position = new Vector3(-15, transform.position.y, transform.position.z);
+        } else if (transform.position.x > 15)
+        {
+            transform.position = new Vector3(15, transform.position.y, transform.position.z);
+        }
+    }
+}
